2325de7d97487ff7ac16be6ba8ab8ee2632b7f1d
LeandroC192 <Leandroc16@nycstudents.net>
2025-03-20 15:48:35 +0000

    completed countIncreasingCols

--- Data.java
+++ Data.java
@@ -7,16 +7,33 @@ public class Data {
         grid = new int[rows][columns];
     }
 
+    public Data (int[][]arr)
+    {
+        grid = arr;
+    }
+
     /**
      * Fills all elements of grid with randomly generated
      * values, as described in part (a)
      * Precondition: grid is not null.
      * grid has at least one element.
      */
-    public void repopulate() {
+    public void repopulate() 
+    {
+        for(int row = 0; row < grid.length; row++)
+        {
+            for(int col = 0; col < grid[0].length; col++)
+            {
+                int val = 0;
+                while(val % 10 != 0 || val % 100 == 0)
+                {
+                    val = (int)(Math.random() * MAX + 1);
+                }
+                grid[row][col] = val;
+            }
+        }
         /* to be implemented in part (a) */
     }
-
 
     /**
      * Returns the number of columns in
@@ -25,7 +42,25 @@ public class Data {
      * Precondition: grid is not null.
      * grid has at least one element.
      */
-    public int countIncreasingCols() {
+    public int countIncreasingCols() 
+    {
+        int count = 0;
+        for(int col = 0; col < grid[0].length; col++)
+        {
+            int increasing = 1;
+            for(int row = 0; row < grid.length; row++)
+            {
+                if(grid[row][col] >= grid[row-1][col])
+                {
+                    increasing++;
+                }
+                if(increasing == grid.length)
+                {
+                    count++;
+                }
+            }
+        }
+        return count;
         /* to be implemented in part (b) */
     }
 
@@ -34,11 +69,13 @@ public class Data {
         String s = "";
         for(int[]row : grid)
         {
-            for(int[col] : grid)
+            for(int col : row)
             {
-                
+                s += col + " ";
             }
+            s += "\n";
         }
+        return s;
     }
 
     // There may be instance variables, constructors
